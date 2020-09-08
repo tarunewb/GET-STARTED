2b2960db1f4f20583b62d73954aba0388840c3c2
tarunewb <tarun.ewb@gmail.com>
2020-09-08 12:40:17 +0530

    submit the data ino json file

--- core/register.php
+++ core/register.php
@@ -1,37 +1,54 @@
 <?php
-$name = $_POST["nam"];
-if (!empty($name)) {
+$name=$_POST["fname"];
+if($name==""){
+    echo "Error......First Name is Empty";
+}
+else{
     echo "$name<br>";
-} else {
-    echo "Error...First name is not Enter<br>";
-    exit;
 }
- 
-$lnam = $_POST["lname"];
-if (!empty($lnam)) {
-    echo "$lnam<br>";
-} else {
+$Name=$_POST["lname"];
+if (!empty($Name)){
+echo "$Name<br>";
+}
+else{
+echo "Error......Last Name is Empty";
+exit;
+}
+$Email=$_POST["mail"];
+if (!empty($Email)){
+echo "$Email<br>";
+}
+else{
+echo "Error......Email Adrress is empty";
+exit;
+}
+$Password=$_POST["password"];
+$cpass=$_POST["cPass"];
+if ($Password==""){
+echo "enter password<br>";
+}
+elseif ($Password!=$cpass){
+echo "Error.... Password does not match<br>";
+}
+else{
 
-    echo "Error... last Name is not enter<br>";
-    exit;
-}
+$file = file_get_contents("../database/user.json");
+$array_data=json_decode($file,true);
+$data=array(
+"name"=>$name,
+"last name"=>$Name,
+"email"=>$Email,
+"password"=>$Password,
+);
  
-$email=$_POST["mail"];
-if($email==""){
-  echo "Error.....enter the email address<br>";
-  exit;
-}else{
-  echo "$email<br>";
+
+
+$array_data[]=$data;
+$encod=json_encode($array_data,true);
+if(file_put_contents("../database/user.json",$encod)){
+echo "Registration Successfull";
+echo '<br>';
 
 }
- 
- $password=$_POST["pwd"];
- $cpassword=$_POST["cpwd"];
- If($password==""){
-   echo "Field Can't be empty<br>";
- }else if($password!=$cpassword){
-   echo "Password don't match<br>";
- } else{ 
-   echo "registration Successful";
- }
-?>
+
+}
